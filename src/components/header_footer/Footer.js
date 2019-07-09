7ec1a5f4c90409c766d179a4e932c4cbe71eee2a
amit88265 <amitkumar88265@gmail.com>
2019-07-09 12:02:10 +0530

    style footer preperly

--- src/components/header_footer/Footer.js
+++ src/components/header_footer/Footer.js
@@ -4,15 +4,13 @@ import './Header_Footer.css';
 const Footer = ()=>{
     return (
         <div className="footer">
-            <div>
-                <img alt="XEFGAME XEBIA" src="logo.png"/>
-                <div className="social_links">
-                    <div>facebook</div>
-                    <div>Google</div>
-                </div>                
+
+            <div className="leftFooter">
+               <p style={{display:'inline-block'}}>XEFXEBIA</p>
+               <a href="#">Xebia</a>                
             </div>
             
-            <div className="other_links">
+            <div className="rightFooter">
                 <a href="/#">About Us</a>
                 <a href="/#">How to Play</a>
                 <a href="/#">Contact Us</a>
