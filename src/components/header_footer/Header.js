abc4d22851c068a588c91645580c109ce53a4312
pranshu1402 <pranshu.ghps@gmail.com>
2019-07-10 15:30:09 +0530

    auth with redux

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -3,10 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/core/Menu';
 import Typography from '@material-ui/core/Typography';
 import './Header_Footer.css';
+import { Avatar } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
     title: {
@@ -17,18 +16,19 @@ const useStyles = makeStyles(theme => ({
 const Header = (props) => {
     const classes = useStyles();
     const loginButton = (<Button color="inherit" href="/auth">LogIn</Button>);
-    const logoutButton = (<Button color="inherit" onClick={props.onLogout}>Log Out</Button>); 
+    const userMenu = (<div className="userMenu">
+        <Avatar/>
+        <Button color="inherit" onClick={props.onLogout}>Log Out</Button>
+    </div>);
+
     return (
             <AppBar>
                 <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="Menu">
-                        <MenuIcon />
-                    </IconButton>
                     <Typography variant="h6" className={classes.title}>
                         XEFGAME
                     </Typography>
                     {
-                        props.isSigned?logoutButton:loginButton
+                        props.isSigned?userMenu:loginButton
                     }
                 </Toolbar>
             </AppBar>
