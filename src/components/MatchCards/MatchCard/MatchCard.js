23e933f5b101b23fb6880ea27557c1e5fd18f5a3
amit88265 <amitkumar88265@gmail.com>
2019-07-10 15:34:54 +0530

    make match card horizontal scrollable and responsive

--- src/components/MatchCards/MatchCard/MatchCard.js
+++ src/components/MatchCards/MatchCard/MatchCard.js
@@ -1,39 +1,87 @@
 import React, { Component } from 'react';
 
-
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
 
 import './MatchCard.css';
 
 
 class DisplayMatch extends Component {
 
+    constructor(props) {
+        super(props);
+        this.dummyData = [
+            {
+                "title": "9 July,2019",
+                "teamA": "IND",
+                "teamB": "AUS"
+            },
+            {
+                "title": "9 July,2019",
+                "teamA": "IND",
+                "teamB": "AUS"
+            },
+            {
+                "title": "9 July,2019",
+                "teamA": "IND",
+                "teamB": "AUS"
+            },
+            {
+                "title": "9 July,2019",
+                "teamA": "IND",
+                "teamB": "AUS"
+            }
+            ,
+            {
+                "title": "9 July,2019",
+                "teamA": "IND",
+                "teamB": "AUS"
+            },
+            ,
+            {
+                "title": "9 July,2019",
+                "teamA": "IND",
+                "teamB": "AUS"
+            }
+        ];
+    }
+
     render() {
         return (
             <div>
-                <Card className="contentCard">
-                    <CardHeader
-                        title="9 July,2019"
-                    />
-                    <CardContent className="contentBody">
+                <GridList cols={3.4} className="gridList" >
+                    {this.dummyData.map(tile => (
+                        <GridListTile className="gridTile" key={tile}>
+                           
+                            <Card className="contentCard">
+                                <CardHeader
+                                    title={tile.title} />
 
-                        <Avatar className="avatar">
-                            IND
-                        </Avatar>
+                                <CardContent className="contentBody">
 
-                        <Typography variant="h6">
-                            ODI
-                        </Typography>
+                                    <Avatar className="avatar">
+                                        {tile.teamA}
+                                    </Avatar>
 
-                        <Avatar className="avatar">
-                            AUS
-                    </Avatar>
-                    </CardContent>
-                </Card>
+                                    <Typography variant="h6">
+                                        ODI
+                                    </Typography>
+
+                                    <Avatar className="avatar">
+                                        {tile.teamB}
+                                    </Avatar>
+
+                                </CardContent>
+                            </Card>
+                        </GridListTile>
+                    ))}
+                </GridList>
+
             </div>
         );
     }
